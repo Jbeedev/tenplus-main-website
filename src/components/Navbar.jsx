b1d85aad466fe343337ce9d7b4655427a4ea87db
Ajibola <ajiquinine@gmail.com>
2022-11-18 15:02:46 +0100

    navlink responsiveness fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,6 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [navMenu, setNavMenu] = useState(false);
 
-
   // const [navMenu, setNavMenu] = useState(false)
 
   const activeLink =
@@ -19,11 +18,12 @@ const Navbar = () => {
     "underline underline-offset-8 decoration-4 decoration-[#01303F] flex items-center";
 
   return (
-    <div className="">
-      <div className="md:block 2xl:items-center mt-[30px] 2xl:flex mx-5 2xl:justify-between ">
-        <div className="md:flex md:justify-between md:items-center md:w-[100%]">
+    <div className=" w-full ">
+
+      <div className="mx-5 md:block 2xl:items-center mt-[30px] flex  justify-between ">
+        <div className="md:flex md:justify-between md:items-center md:w-full">
           <img
-            className="md:w-[112px] md:h-[30px] 2xl:w-[161px] 2xl:h-[43px]"
+            className=" 2xl:max-w-[161px] 2xl:max-h-[43px]"
             src={TenPlusLogo}
             alt="tenplus logo"
           />
@@ -38,7 +38,7 @@ const Navbar = () => {
           </div>
         </div>
         <div
-          className={`md:bg-white md:rounded md:py-5 md:border border-black md:text-black md:mr-0 2xl:mr-[373px] ${
+          className={`md:bg-white md:rounded md:py-5 md:border border-black md:text-black md:mr-0 ${
             isOpen ? "md:block" : "md:hidden"
           }`}
         >
@@ -55,53 +55,85 @@ const Navbar = () => {
                 className={({ isActive }) =>
                   isActive ? activeLinkSolution : "flex items-center"
                 }
-                onClick={() => setNavMenu(!navMenu) }
+                onClick={() => setNavMenu(!navMenu)}
               >
                 Solutions{" "}
                 <span className="text-2xl">
                   <RiArrowDropDownLine />
                 </span>
               </NavLink>
-              { navMenu && <div className=" absolute top-[86px] bg-white" style={{color: "#01303F"}}>
-                <ul className=" p-7 text-base font-normal ">
-                  <Link to="/solutions/web" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Web Development</li>
-                  </Link>
-                  <Link to="/solutions/app" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Mobile App Development</li>
-                  </Link>
-                  <Link to="/solutions/graphics" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Graphics Design</li>
-                  </Link>
-                  <Link to="/solutions/digital" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Digital Marketing</li>
-                  </Link>
-                  <Link to="/solutions/ai" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Artificial Intelligence</li>
-                  </Link>
-                  <Link to="/solutions/project" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Project Management</li>
-                  </Link>
-                  <Link to="/solutions/printing" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Printing Services</li>
-                  </Link>
-                  <Link to="/solutions/video" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Video Editing</li>
-                  </Link>
-                  <Link to="/solutions/cyber" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Cybersecurity</li>
-                  </Link>
-                  <Link to="/solutions/accessories" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Blockchain</li>
-                  </Link>
-                  <Link to="/solutions/accessories" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">Computers and Accessories</li>
-                  </Link>
-                  <Link to="/solutions/training" onClick={() => setNavMenu(false) }>
-                    <li className="mb-2">IT Training</li>
-                  </Link>
-                </ul>
-              </div>}
+              {navMenu && (
+                <div
+                  className=" absolute top-[86px] bg-white"
+                  style={{ color: "#01303F" }}
+                >
+                  <ul className=" p-7 text-base font-normal ">
+                    <Link to="/solutions/web" onClick={() => setNavMenu(false)}>
+                      <li className="mb-2">Web Development</li>
+                    </Link>
+                    <Link to="/solutions/app" onClick={() => setNavMenu(false)}>
+                      <li className="mb-2">Mobile App Development</li>
+                    </Link>
+                    <Link
+                      to="/solutions/graphics"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Graphics Design</li>
+                    </Link>
+                    <Link
+                      to="/solutions/digital"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Digital Marketing</li>
+                    </Link>
+                    <Link to="/solutions/ai" onClick={() => setNavMenu(false)}>
+                      <li className="mb-2">Artificial Intelligence</li>
+                    </Link>
+                    <Link
+                      to="/solutions/project"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Project Management</li>
+                    </Link>
+                    <Link
+                      to="/solutions/printing"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Printing Services</li>
+                    </Link>
+                    <Link
+                      to="/solutions/video"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Video Editing</li>
+                    </Link>
+                    <Link
+                      to="/solutions/cyber"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Cybersecurity</li>
+                    </Link>
+                    <Link
+                      to="/solutions/accessories"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Blockchain</li>
+                    </Link>
+                    <Link
+                      to="/solutions/accessories"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">Computers and Accessories</li>
+                    </Link>
+                    <Link
+                      to="/solutions/training"
+                      onClick={() => setNavMenu(false)}
+                    >
+                      <li className="mb-2">IT Training</li>
+                    </Link>
+                  </ul>
+                </div>
+              )}
             </div>
             <NavLink
               to="/blog"
